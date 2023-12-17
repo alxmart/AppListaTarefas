68a8a796862628fe28a35012c12935ff069b09b5
alxmart <alxmartinez@gmail.com>
2023-12-17 19:58:16 -0300

    Created interface ITarefaDAO .

---
+++ app/src/main/java/com/luizafmartinez/applistatarefas/database/ITarefaDAO.kt
@@ -0,0 +1,10 @@
+package com.luizafmartinez.applistatarefas.database
+
+import com.luizafmartinez.applistatarefas.model.Tarefa
+
+interface ITarefaDAO {
+    fun salvar( tarefa: Tarefa): Boolean
+    fun atualizar( tarefa: Tarefa): Boolean
+    fun salvar( idTarefa: Int): Boolean
+    fun listar( ): List<Tarefa>
+}
