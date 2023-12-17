531ad5524b6a8b80d1fffd4976417b6161cb455a
alxmart <alxmartinez@gmail.com>
2023-12-17 20:12:33 -0300

    Finished code fun salvar()

---
+++ app/src/main/java/com/luizafmartinez/applistatarefas/database/TarefaDAO.kt
@@ -0,0 +1,51 @@
+package com.luizafmartinez.applistatarefas.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.util.Log
+import com.luizafmartinez.applistatarefas.model.Tarefa
+
+class TarefaDAO(context: Context) : ITarefaDAO {
+
+    private val escrita = DatabaseHelper( context ).writableDatabase
+    private val leitura = DatabaseHelper( context ).readableDatabase
+
+    override fun salvar(tarefa: Tarefa): Boolean {
+
+        val conteudos = ContentValues()
+
+        conteudos.put("${DatabaseHelper.COLUNA_DESCRICAO}", tarefa.descricao)
+
+        try {
+            escrita.insert(
+                DatabaseHelper.NOME_TABELA_TAREFAS,
+                null,
+                conteudos
+            )
+            Log.i("info_db", "Sucesso ao salvar tarefa")
+        } catch (e: Exception  ) {
+            e.printStackTrace()
+            Log.i("info_db", "Erro ao salvar a tarefa")
+            return false
+        }
+        return true
+    }
+
+
+    override fun salvar(idTarefa: Int): Boolean {
+
+
+    }
+
+    override fun atualizar(tarefa: Tarefa): Boolean {
+
+
+    }
+
+    override fun listar(): List<Tarefa> {
+
+
+    }
+
+
+}
