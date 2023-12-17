6868e77c9f671a2872aafaaf790d678cc11036bf
alxmart <alxmartinez@gmail.com>
2023-12-17 19:44:47 -0300

    onCreate function's code to create Database.

--- app/src/main/java/com/luizafmartinez/applistatarefas/database/DatabaseHelper.kt
+++ app/src/main/java/com/luizafmartinez/applistatarefas/database/DatabaseHelper.kt
@@ -1,8 +1,40 @@
 package com.luizafmartinez.applistatarefas.database
 
-class DatabaseHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(
+    context, NOME_BANCO_DADOS, null, VERSAO
+)  {
+    companion object {
+        const val NOME_BANCO_DADOS = "ListaTarefas.db"
+        const val VERSAO = 1
+        const val NOME_TABELA_TAREFAS = "tarefas"
+        const val COLUNA_ID_TAREFA = "id_tarefa"
+        const val COLUNA_DESCRICAO = "descricao"
+        const val COLUNA_DATA_CADASTRO = "data_cadastro"
+    }
 
+    override fun onCreate(db: SQLiteDatabase?) {
 
+        val sql = "CREATE TABLE IF NOT EXISTS $NOME_TABELA_TAREFAS (" +
+            "$COLUNA_ID_TAREFA INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+            "$COLUNA_DESCRICAO VARCHAR(70)," +
+            "$COLUNA_DATA_CADASTRO DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP" +
+            ");"
+        try {
+            db?.execSQL( sql )
+            Log.i("info_db", "Sucesso ao criar a tabela")
+        } catch (e: Exception  ) {
+            e.printStackTrace()
+            Log.i("info_db", "Erro ao criar a tabela")
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
 
 }
---
+++ app/src/main/java/com/luizafmartinez/applistatarefas/model/Tarefa.kt
@@ -0,0 +1,9 @@
+package com.luizafmartinez.applistatarefas.model
+
+data class Tarefa(
+    val idTarefa: Int,
+    val descricao: String,
+    val dataCadastro: String
+) {
+
+}
