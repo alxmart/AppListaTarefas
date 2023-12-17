8512e60819bb6b8013169be0bfe387b6d33ab505
alxmart <alxmartinez@gmail.com>
2023-12-17 18:17:04 -0300

    AdicionarTarefaActivity created, FAB setOnclicklistener defined.

--- app/src/main/java/com/luizafmartinez/applistatarefas/AdicionarTarefaActivity.kt
+++ app/src/main/java/com/luizafmartinez/applistatarefas/AdicionarTarefaActivity.kt
@@ -1,23 +1,18 @@
 package com.luizafmartinez.applistatarefas
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luizafmartinez.applistatarefas.databinding.ActivityAdicionarTarefaBinding
 import com.luizafmartinez.applistatarefas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class AdicionarTarefaActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityAdicionarTarefaBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.fabAdicionar.setOnClickListener{
-            val intent = Intent(this, AdicionarTarefaActivity::class.java)
-            startActivity( intent )
-        }
     }
 }
